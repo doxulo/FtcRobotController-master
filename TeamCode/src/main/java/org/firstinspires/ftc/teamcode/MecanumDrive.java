38d003caba8f12ae3453a2c7c53974d6cb0fce97
scary787 <mtitterton7@gmail.com>
2021-10-21 14:47:22 -0500

    mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -9,32 +9,63 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp
 public class MecanumDrive extends LinearOpMode {
 
+    /**
+     * Initialize all motors that control the robot's wheels
+     *
+     * RF - Right Front wheel
+     * RB - Right Back wheel
+     * LF - Left Front wheel
+     * LB - Left Back wheel
+     */
     public DcMotor LF; // 1
     public DcMotor RF; // 0
     public DcMotor LB; // 3
     public DcMotor RB; // 2
 
+    public DcMotor Duck_Wheel;
+
+    public DcMotor Intake;
+
+    /**
+     * Applies the needed power to move the robot
+     *
+     * @param y     First Variable used in the calculation of the power
+     * @param x     Second variable used in the calculations of the power
+     * @param rx    Third variable used in the calculations of the power
+     */
     public void mecanum(double y, double x, double rx) {
-        //change if strafe bad
-        double STRAFING_CORRECTION = 1;
+        // Change if strafe bad
+        double STRAFING_CORRECTION = 1.0D;
+        // TODO: Change gamepad1.left_stick_x to x?
         x = gamepad1.left_stick_x * STRAFING_CORRECTION;
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double LF_power = (y + x + rx) / denominator;
-        double LB_power = (y - x + rx) / denominator;
-        double RF_power = (y - x - rx) / denominator;
-        double RB_power = (y + x - rx) / denominator;
+        double LF_power = (y + x + rx) / denominator/2;
+        double LB_power = (y - x + rx) / denominator/2;
+        double RF_power = (y - x - rx) / denominator/2;
+        double RB_power = (y + x - rx) / denominator/2;
+        //denominator/power = speed
 
         LF.setPower(LF_power);
         RF.setPower(RF_power);
         LB.setPower(LB_power);
         RB.setPower(RB_power);
     }
+
+    /**
+     * Applies the same power to all of the wheels
+     *
+     * @param power     The power to be set to all of the motors
+     */
     public void wheels_power(double power) {
         LF.setPower(power);
         RB.setPower(power);
         LB.setPower(power);
         RF.setPower(power);
     }
+
+    /**
+     * Main method that executes upon code run
+     */
     @Override
     public void runOpMode() {
         LF = hardwareMap.dcMotor.get("LF");
@@ -44,7 +75,7 @@ public class MecanumDrive extends LinearOpMode {
 
         //right side motors are reversed, change to left side if needed
         LF.setDirection(DcMotorSimple.Direction.FORWARD);
-        RF.setDirection(DcMotorSimple.Direction.FORWARD);//r
+        RF.setDirection(DcMotorSimple.Direction.FORWARD);
         RB.setDirection(DcMotorSimple.Direction.REVERSE);//r
         LB.setDirection(DcMotorSimple.Direction.FORWARD);
 
@@ -57,6 +88,16 @@ public class MecanumDrive extends LinearOpMode {
         RF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         RB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         LB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        Duck_Wheel = hardwareMap.dcMotor.get("Duck_Wheel");
+        Duck_Wheel.setDirection(DcMotorSimple.Direction.FORWARD);
+        Duck_Wheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Duck_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        Intake = hardwareMap.dcMotor.get("Intake");
+        Intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        Intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         waitForStart();
         while (opModeIsActive()) {
@@ -72,6 +113,21 @@ public class MecanumDrive extends LinearOpMode {
 
             //drive train
             mecanum(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+
+            if (gamepad1.x) {
+                // try the power on the ducks
+                Duck_Wheel.setPower(.9);
+            }
+            else if (gamepad1.y) {
+                Duck_Wheel.setPower(0);
+            }
+
+            if (gamepad1.a) {
+                Intake.setPower(1);
+            }
+            else if (gamepad1.b) {
+                Intake.setPower(0);
+            }
         }
     }
 }
