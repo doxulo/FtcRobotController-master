825546813399e0e0b2f6679cfa322040e2f17e00
dxl-1805 <72246173+dxl-1805@users.noreply.github.com>
2022-01-20 01:47:20 -0600

    Mít mít Tét ting

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,4 +1,44 @@
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.util.Vector;
+
+import sun.font.TrueTypeFont;
+
 public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(500);
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(40, 10, 3.14, Math.toRadians(90), 30)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, Math.toRadians(270)))
+                                .setReversed(true)
+                                .splineToConstantHeading(new Vector2d(22, 25), Math.toRadians(90))
+                                .setReversed(false)
+                                .splineToSplineHeading(new Pose2d(0, 0, Math.toRadians(180)), Math.toRadians(200))
+                                .setReversed(true)
+                                .forward(36)
+                                .back(36)
+                                .splineToSplineHeading(new Pose2d(22, 25, Math.toRadians(270)), Math.toRadians(90))
+                                .setReversed(false)
+                                .splineToSplineHeading(new Pose2d(0, 0, Math.toRadians(180)), Math.toRadians(200))
+                                .setReversed(true)
+                                .forward(36)
+                                .build()
+                );
+        meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
 }
