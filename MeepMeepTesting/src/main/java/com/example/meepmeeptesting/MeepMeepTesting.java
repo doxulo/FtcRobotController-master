576e91cc2c37ca68fd675bcda085259030f324fc
dxl-1805 <72246173+dxl-1805@users.noreply.github.com>
2022-03-11 08:39:11 -0600

    Blue Cycles w/o Slide

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,7 +1,6 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
@@ -9,8 +8,6 @@ import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
-
-import java.util.Vector;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
@@ -118,6 +115,39 @@ public class MeepMeepTesting {
                                 .splineToSplineHeading(new Pose2d(40, -67, Math.toRadians(0)),0)
                                 .build()
                 );
+
+        RoadRunnerBotEntity blue_cycle_no_slide = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setColorScheme(new ColorSchemeBlueLight())
+                .setConstraints(50, 50, 2 * Math.PI, 2 * Math.PI, 14.5)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(10, 65, Math.toRadians(90)))
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(-12, 42, Math.toRadians(90)), Math.toRadians(270))
+                                .setReversed(false)
+                                .splineToSplineHeading(new Pose2d(22, 67, Math.toRadians(0)), Math.toRadians(0))
+                                .splineToSplineHeading(new Pose2d(40, 67, Math.toRadians(0)),0)
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(22,67, Math.toRadians(0)), Math.toRadians(180))
+                                .splineToSplineHeading(new Pose2d(-12, 42, Math.toRadians(90)), Math.toRadians(270))
+                                .setReversed(false)
+                                .splineToSplineHeading(new Pose2d(22, 67, Math.toRadians(0)), Math.toRadians(0))
+                                .splineToSplineHeading(new Pose2d(40, 67, Math.toRadians(0)),0)
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(22,67, Math.toRadians(0)), Math.toRadians(180))
+                                .splineToSplineHeading(new Pose2d(-12, 42, Math.toRadians(90)), Math.toRadians(270))
+                                .setReversed(false)
+                                .splineToSplineHeading(new Pose2d(22, 67, Math.toRadians(0)), Math.toRadians(0))
+                                .splineToSplineHeading(new Pose2d(40, 67, Math.toRadians(0)),0)
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(22,67, Math.toRadians(0)), Math.toRadians(180))
+                                .splineToSplineHeading(new Pose2d(-12, 42, Math.toRadians(90)), Math.toRadians(270))
+                                .setReversed(false)
+                                .splineToSplineHeading(new Pose2d(22, 67, Math.toRadians(0)), Math.toRadians(0))
+                                .splineToSplineHeading(new Pose2d(40, 67, Math.toRadians(0)),0)
+                                .build()
+                );
+
         RoadRunnerBotEntity blue_duckbox = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setColorScheme(new ColorSchemeBlueDark())
@@ -162,7 +192,7 @@ public class MeepMeepTesting {
         meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_OFFICIAL)
                 .setDarkMode(false)
                 .setBackgroundAlpha(1f)
-                .addEntity(blue_cycle)
+                .addEntity(blue_cycle_no_slide)
                 .addEntity(red_cycle)
                 .addEntity(red_duckbox)
                 .addEntity(blue_duckbox)
